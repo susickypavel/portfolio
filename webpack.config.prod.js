2f0e5155352cfbb26666c40e083c192b3fd1ad60
Thesoreon <pavels.sus@gmail.com>
2018-12-08 17:15:25 +0100

    fixed routing problem, second production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,7 +11,7 @@ module.exports = {
     output: {
         filename: "[name].[hash].js",
         path: path.resolve(__dirname, "dist"),
-        publicPath: "/"
+        publicPath: "./"
     },
 
     resolve: {
